8eb5624b2aa1c1e209b07f561bbeb7ed2c12c997
David <davidhiggins1712@gmail.com>
2017-02-07 23:45:13 +0000

    M

--- webscraper/bet355_bet_history_scraper.py
+++ webscraper/bet355_bet_history_scraper.py
@@ -5,8 +5,6 @@ import io
 
 
 def main():
-    bets = []
-
     driver = webdriver.Firefox(executable_path='./geckodriver')
     driver.get("https://www.bet365.com/?lng=1&cb=10326429708#/HO/")
 
@@ -26,13 +24,14 @@ def main():
 
     all_bets_from_last_six_months = bets_last_two_days + bets_last_six_months
 
-    with io.open('bets.json', 'w', encoding="utf-8") as f:
-        f.write(unicode(json.dumps(all_bets_from_last_six_months, ensure_ascii=False)))
+    output = {
+        'bets': []
+    }
 
-    with io.open('bets.json', encoding="utf-8") as f:
-        data = json.load(f)
-        for ind, b in enumerate(data):
-            print("bet #" + str(ind + 1) + " :  " + b['bet_result'])
+    output['bets'] = all_bets_from_last_six_months
+
+    with io.open('bets.json', 'w', encoding="utf-8") as f:
+        f.write(unicode(json.dumps(output, ensure_ascii=False)))
 
 
 if __name__ == '__main__':
